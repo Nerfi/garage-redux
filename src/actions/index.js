3a569dff76cad2209a3a88cf4eae70ebcf628cf8
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-11 14:09:33 +0200

    adding action to the created new car new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,6 @@
 // TODO: add and export your own actions
 
-import { withRouter } from 'react-router-dom'
-// this also works with react-router-native
-// no fucking idea how this works
-const Button = withRouter(({ history }) => (
-  <button
-    type='button'
-    onClick={() => { history.push('/new-location') }}
-  >
-    Click Me!
-  </button>
-))
+
 
 
 
@@ -18,6 +8,7 @@ const BASE_URL = 'https:wagon-garage-api.herokuapp.com/my-awesome-garage/cars';
 
 export const FETCH_CARS = 'FETCH_CARS';
 export const CAR_SELECTED = 'CAR_SELECTED';
+export const CAR_CREATED = 'CAR_CREATED';
 
 export function fetchCars (car) {
 const url = '${BASE_URL}/${car}/cars';
@@ -34,4 +25,9 @@ export function selectedCar(car) {
     type: SELECTED_CAR,
     payload: car // we return the car itself
   };
+}
+
+
+export function createCar(body) {
+
 }
