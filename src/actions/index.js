f34aca447a80d61eb877e05d34b18bb9e82d044a
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-11 14:01:37 +0200

    no fucking idea what to do, I hate coding  Ñ

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,37 @@
 // TODO: add and export your own actions
+
+import { withRouter } from 'react-router-dom'
+// this also works with react-router-native
+// no fucking idea how this works
+const Button = withRouter(({ history }) => (
+  <button
+    type='button'
+    onClick={() => { history.push('/new-location') }}
+  >
+    Click Me!
+  </button>
+))
+
+
+
+const BASE_URL = 'https:wagon-garage-api.herokuapp.com/my-awesome-garage/cars';
+
+export const FETCH_CARS = 'FETCH_CARS';
+export const CAR_SELECTED = 'CAR_SELECTED';
+
+export function fetchCars (car) {
+const url = '${BASE_URL}/${car}/cars';
+const promise = fetch(url).then(r => r.json());
+  return {
+    type: FETCH_CARS,
+    payload: promise
+  };
+}
+
+
+export function selectedCar(car) {
+  return {
+    type: SELECTED_CAR,
+    payload: car // we return the car itself
+  };
+}
--- src/index.jsx
+++ src/index.jsx
@@ -9,18 +9,51 @@ import { createHistory as history } from 'history';
 
 import '../assets/stylesheets/application.scss';
 
+//importing reducers
+
+import garageReducer from './reducers/garage_reducer';
+import carsReducer from './reducers/cars_reducer';
+
+//importing the form
+import { reducer as formReducer } from 'redux-form';
+
+//importing navigation after API
+import { Route } from 'react-router-dom';
+
+
+//wagon
+
+const garageName = prompt("what is your garage?") || 'garage${Math.floor(10 + (Math.random() * 90))}';
+
+const  initialState = {
+garage: garageName, // why we need garage name in the state?
+cars: [
+  { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+  { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+  { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+  { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+]
+
+//good practise
+
+const store = createStore(reducers, initialState, middlewares);
+
 const reducers = combineReducers({
-  // key: reducer
+ garage: garageReducer,
+ cars: carsReducer
+ form: formReducer // idk if it's ok
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <Router history={history}>
       <Switch>
-        TODO
+      <Route path="/:index" component={CarsIndex} />
+      <Route path="/:new" component={CarsNew} />
+
       </Switch>
     </Router>
   </Provider>,
