97b42aed3dc522995b7fd43131ff7df2b0f848a6
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-06-15 18:18:12 +0200

    finish added solution

--- src/containers/cars_show.jsx
+++ src/containers/cars_show.jsx
@@ -48,3 +48,73 @@ class CarsShow extends Component {
 export default reduxForm({ form: 'showCarForm' })(
  connect(null, { CarsShow })(CarsShow)
 );
+
+
+//solution
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+
+import Aside from '../components/aside';
+import { removeCar } from '../actions';
+
+
+class CarsShow extends Component {
+  handleClick = () => {
+    this.props.removeCar(this.props.history, this.props.car);
+
+  }
+
+  render() {
+    const car = this.props.car;
+    if(!car) {
+      return(
+          <Aside key="aside" garage={this.props.garage}>
+          <Link to="/">Back to list</Link>
+        </Aside>);
+
+
+        )
+    }
+
+    return [
+    <Aside key="aside" garage={this.props.garage}>
+        <Link to="/">Back to list</Link>
+      </Aside>,
+      <div className="car-container" key="car">
+        <div className="car-card">
+          <img className="car-picture" src="/assets/images/logo_square.svg" />
+          <div className="car-details">
+            <span>{car.brand} - {car.model}</span>
+            <ul>
+              <li><strong>Owner:</strong> {car.owner}</li>
+            </ul>
+            <span className="plate">{car.plate}</span>
+          </div>
+          <button className="delete" onClick={this.handleClick}>
+            <i className="fa fa-trash-o" aria-hidden="true"></i>
+            Delete
+          </button>
+        </div>
+      </div>
+
+    ];
+  }
+};
+
+
+function mapStateToProps(state, ownProps) {
+
+  const id = ParseInt(ownProps.match.params.id);
+  return {
+    car: state.cars.find((car) => car.id === id),
+  };
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({removeCar}, dispatch);
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CarsShow));
